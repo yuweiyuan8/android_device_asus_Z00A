fe84babdaf1598d0aeda001e863d9d1d9d0ebc7a
Yu weiyuan <32944372+yuweiyuan8@users.noreply.github.com>
2023-09-29 14:00:34 +0800

    Update Android.mk

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := librecovery_updater_mofd
 LOCAL_SRC_FILES := recovery_updater.cpp fw_version_check.cpp
-LOCAL_C_INCLUDES += bootable/recovery
+LOCAL_C_INCLUDES := bootable/recovery
 
 include $(BUILD_STATIC_LIBRARY)
